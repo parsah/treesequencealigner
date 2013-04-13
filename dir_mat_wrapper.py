8e3fe18fd3af0d932d7054318dccb1dd374fa3f1
Todd Gillette <todd.gillette@gmail.com>
2013-04-13 17:58:58 -0300

    Added transpose capability

--- dir_mat_wrapper.py
+++ dir_mat_wrapper.py
@@ -4,8 +4,18 @@ A wrapper for directional score matrices
 
 class DirMatWrapper():
 
-  def __init__(self,nrows,ncols):
+	def __init__(self,nrows,ncols,T=None):
 		self.nrows = nrows
 		self.ncols = ncols
-		self.score = IntMatrix(nrows,ncols)
-		self.flag = IntMatrix(nrows,ncols)
+		if T is None:
+			self.score = IntMatrix(nrows,ncols)
+			self.flag = IntMatrix(nrows,ncols)
+			self.T = DirMatWrapper(nrows,ncols,self)
+		else:
+			self.T = T
+			self.score = T.score.T
+			self.flag = T.flag.T
+
+	def transpose(self):
+		return self.T
+		
