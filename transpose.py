4689b68a75a6aabb6da27668390a88853466f31d
Todd Gillette <todd.gillette@gmail.com>
2013-04-15 16:33:58 -0300

    added set_data method

--- transpose.py
+++ transpose.py
@@ -4,12 +4,15 @@ Works with Matrix classes to provide low cost matrix transposition.
 
 class Transpose():
 
-  def __init__(self,matrix):
+	def __init__(self,matrix):
 		self.transpose = matrix
 		self.T = matrix
 		
 	def getData(self,i,j):
 		return self.transpose.data[j][i]
+
+	def setData(self,i,j,val):
+		self.transpose.data[j][i] = val
 
 	def transpose(self):
 		return self.T
