89508f567b39bbc020c53e98d523ed5ee0bd2c94
Todd Gillette <todd.gillette@gmail.com>
2013-04-15 16:30:28 -0300

    added set_data method

--- int_matrix.py
+++ int_matrix.py
@@ -5,29 +5,24 @@ A high-level class used for modeling a Matrix data-structure.
 from transpose import Transpose
 
 class IntMatrix():
-    ''' 
-    A Matrix is your traditional multi-dimensional array whereby its 
-    contents are indexed by respective integers.
-    '''
-    def __init__(self, nrows, ncols):
-        self.nrows = nrows
-        self.ncols = ncols
-        self.data = [[0 for _ in range(self.ncols)] 
+	''' 
+	A Matrix is your traditional multi-dimensional array whereby its 
+	contents are indexed by respective integers.
+	'''
+	def __init__(self, nrows, ncols):
+		self.nrows = nrows
+		self.ncols = ncols
+		self.data = [[0 for _ in range(self.ncols)] 
                      for _ in range(self.nrows)]
-  	self.transpose = Transpose(self)
+		self.transpose = Transpose(self)
 		self.T = self.transpose
     
-    def get_data(self,i,j):
-        return self.data[i][j]
+	def get_data(self,i,j):
+		return self.data[i][j]
+	
+	def set_data(self,i,j,val):
+		self.data[i][j] = val
     
-		return self.transpose
 	def transpose(self):
-	
-    def debug(self):
-        ''' 
-        Useful method to print-out an entire matrix row-by-row.
-        '''
-        for rownum in range(self.nrows):
-            print(self.data[rownum], self.state[rownum])
-            
-	
+		return self.transpose
+		
