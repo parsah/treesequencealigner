6c1b247567ce63825d5454e559490080857a9bbf
Todd Gillette <todd.gillette@gmail.com>
2013-04-22 18:39:10 -0300

    fix tabs

--- matrix.py
+++ matrix.py
@@ -3,17 +3,17 @@ A high-level class used for modeling a Matrix data-structure.
 '''
 
 class Matrix():
-    ''' 
-    A Matrix is your traditional multi-dimensional array whereby its 
-    contents are indexed by respective integers.
-    '''
-    def __init__(self, nrows, ncols):
-        self.nrows = nrows
-        self.ncols = ncols
-        self._data = [[0.0 for _ in range(self.ncols)] 
+	''' 
+	A Matrix is your traditional multi-dimensional array whereby its 
+	contents are indexed by respective integers.
+	'''
+	def __init__(self, nrows, ncols):
+		self.nrows = nrows
+		self.ncols = ncols
+		self._data = [[0.0 for _ in range(self.ncols)] 
                      for _ in range(self.nrows)]
     
-    def get_data(self,i,j):
+	def get_data(self,i,j):
 		return self._data[i][j]
 	
 	def set_data(self,i,j,val):
@@ -22,13 +22,13 @@ class Matrix():
 	def transpose(self):
 		return self.transpose
     
-    def debug(self):
-        ''' 
-        Useful method to print-out an entire matrix row-by-row.
-        '''
-        for rownum in range(self.nrows):
-            print(self._data[rownum], self._state[rownum])
-            
+	def debug(self):
+		''' 
+		Useful method to print-out an entire matrix row-by-row.
+		'''
+		for rownum in range(self.nrows):
+			print(self._data[rownum], self._state[rownum])
+
 #if __name__ == '__main__':
 #    m = Matrix(nrows=5, ncols=2)
 #    m.data[0][0] = 12
